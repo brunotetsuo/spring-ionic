af2eca7c90fec438209e6b40f008bc9a0fb08cbf
Bruno Tetsuo <mundoct@hotmail.com>
2021-01-01 19:37:14 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/brunotetsuo/springionic/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.brunotetsuo.springionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brunotetsuo.springionic.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
