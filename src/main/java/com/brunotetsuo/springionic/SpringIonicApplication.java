0282a49e564f86cc79fb5e591238fa9da10c3f13
Bruno Tetsuo <mundoct@hotmail.com>
2021-01-01 15:39:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/brunotetsuo/springionic/SpringIonicApplication.java
+++ src/main/java/com/brunotetsuo/springionic/SpringIonicApplication.java
@@ -1,13 +1,32 @@
 package com.brunotetsuo.springionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.brunotetsuo.springionic.domain.Categoria;
+import com.brunotetsuo.springionic.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringIonicApplication {
+public class SpringIonicApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
