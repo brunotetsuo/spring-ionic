664b019cca165d71ffea724c08d3d336e0dbca57
Bruno Tetsuo <mundoct@hotmail.com>
2021-01-03 13:48:37 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/brunotetsuo/springionic/services/CategoriaService.java
+++ src/main/java/com/brunotetsuo/springionic/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.brunotetsuo.springionic.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.brunotetsuo.springionic.domain.Categoria;
 import com.brunotetsuo.springionic.repositories.CategoriaRepository;
+import com.brunotetsuo.springionic.services.exceptions.DataIntegrityException;
 import com.brunotetsuo.springionic.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
---
+++ src/main/java/com/brunotetsuo/springionic/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,14 @@
+package com.brunotetsuo.springionic.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
